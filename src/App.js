d9a07417472ebe217981783d311e057abae3ddaf
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-12-10 19:38:54 +0300

    header init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <a href="" class="logo">
+          Gigly Puff
         </a>
+        <nav class="navbar">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Skills</a>
+          <a href="">Projects</a>
+        </nav>
       </header>
     </div>
   );
